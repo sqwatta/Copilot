1f23ae36bdf8e8487db8a96e60b4fa4b6df8fd82
miguelapan <116359144+miguelapan@users.noreply.github.com>
2025-06-11 21:26:57 +0200

    grymma uppdateringar

--- goran-arborist/app/page.tsx
+++ goran-arborist/app/page.tsx
@@ -1,11 +1,40 @@
+'use client';
 
+import { useState } from "react";
 
 export default function Home() {
+
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const clickHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setMessage(`VI ÅTERKOMMER TILLBAKA TILL DIN EMAIL: ${email} MED SVAR PÅ DITT ÄRENDE: ${subject}`);
+    setTimeout(() => {
+      setMessage('');
+    }, 5000);
+    setEmail('');
+    setSubject('');
+  };
+
   return (
-    
-    <div>
+    <div className="wrapper">
+      <div className="header">
       <h1>Tjena Tjena!</h1>
       <p>Så var det med det!</p>
+      </div>
+
+      <form className="form-contact" onSubmit={clickHandler}>
+        <label htmlFor="email">email:</label>
+        <input type="text" id="email" name="email" onChange={(e) => setEmail(e.target.value)} />
+
+        <label htmlFor="subject">ärende:</label>
+        <textarea id="subject" rows={10} name="subject" onChange={(e) => setSubject(e.target.value)} />
+        <button type="submit">Skicka</button>
+
+        {message && <p className="message">{message}</p>}
+      </form>
     </div>
   );
 }
